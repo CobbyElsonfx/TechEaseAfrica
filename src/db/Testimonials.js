1148d6c366de10b2b6d6f3d75c956fd05a0e3adc
Francis Andoh <andohfrancis9187@gmail.com>
2024-10-07 16:49:11 +0000

    added new changes

--- src/db/Testimonials.js
+++ src/db/Testimonials.js
@@ -6,28 +6,23 @@ import GladysAkuaku from "../assets/GladysAkuaku.jpg"
 const testimonials = [
     {
       text: "Taking the Social Media Marketing course at Techease Africa has been a game-changer for me. The course is practical, hands-on, and tailored to today's digital landscape. The instructors were knowledgeable and approachable, providing real-world strategies that I could implement immediately. Whether you're a beginner or looking to refine your skills, this course will equip you with the tools and confidence needed to succeed. I highly recommend it to anyone looking to enhance their digital marketing expertise!",
-      name: "Peace Awotwe",
-      title: "CTO of Company Co.",
+      name: "Gladys Akuaku",
+      title: "Digital Marketer",
+      image:  GladysAkuaku,
+    },
+    {
+      text: "Completing the Advanced Data Science and Analytics course at Techease Africa was an incredible experience. Over the 2-month period, I gained deep insights into data analysis, machine learning, and predictive modeling through hands-on projects and expert guidance. The curriculum was well-structured, and the instructors were always available to help with challenging concepts. This course has equipped me with the skills to solve real-world problems using data, and I would highly recommend it to anyone serious about advancing their career in data science!.",
+      name: "Lois Veracious",
+      title: "Developer",
       image: PeaceAwotweImg,
     },
     {
-      text: "Taking the Advanced Website Development course at Techease Africa was a fantastic journey. In just 2 months, I mastered complex web development concepts, from frontend to backend, and worked on real-world projects that boosted my confidence. The instructors were incredibly supportive, providing in-depth explanations and guidance every step of the way. If you're serious about becoming a skilled web developer, this course is a must. I highly recommend it to anyone looking to take their web development skills to the next level!.",
-      name: "Leroy Jenkins",
-      title: "CTO of Company Co.",
-      image: "https://source.unsplash.com/50x50/?portrait?2",
-    },
-    {
       text: "Completing the Advanced Graphic Design course at Techease Africa was an enriching experience. Over the 2 months, I honed my design skills and learned how to create visually compelling projects with industry-standard tools. The course was hands-on, with expert guidance from experienced instructors who provided personalized feedback. I highly recommend this course to anyone looking to elevate their graphic design abilities and produce professional-level work!",
-      name: "Gladys Akuaku -Social media marketing",
-      title: "CTO of Company Co.",
+      name: "Afram Ebenezer",
+      title: "Graphic Desginer",
       image: GladysAkuaku,
     },
-    {
-      text: "Te omnes virtute volutpat sed. Ei esse eros interesset vel, ei populo denique ocurreret vix, eu cum pertinax mandamus vituperatoribus. Solum nihil luptatum per ex, ei amet viderer eos. Ea illum labitur mnesarchum pro. Eius meis salutandi ei nam, alterum expetenda et nec. Expetenda intellegat at eum, per mazim sanctus honestatis ad. Ei noluisse invenire vix. Te ancillae patrioque qui, probo bonorum vivendum ex vim.",
-      name: "Leroy Jenkins",
-      title: "CTO of Company Co.",
-      image: "https://source.unsplash.com/50x50/?portrait?4",
-    },
+
   ];
 
 export default testimonials
